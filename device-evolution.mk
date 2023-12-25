de8574610d87d2bef22eda03e27912076b0e39e6
AnierinB <anierin@evolution-x.org>
2023-12-24 16:13:46 -0800

    shusky: Ship PixelParts
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device-evolution.mk
+++ device-evolution.mk
@@ -21,6 +21,9 @@ PRODUCT_PACKAGES += \
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/shusky-kernel/Image.lz4
 
+# PixelParts
+include packages/apps/PixelParts/device.mk
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
